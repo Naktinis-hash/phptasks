b0561904f4c8d85a9c1ad6b462d397730f58e34a
Naktinis-hash <57089674+Naktinis-hash@users.noreply.github.com>
2019-10-29 10:19:17 +0000

    html uzrasymas

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
-<?php
-print 'Veikia!';
+<!DOCTYPE html>
+<!--
+$(SRC_HEADER,indent("   "))
+  -->
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" media="screen, print"
+        href="$(NAME).css" />
+  <script type="text/javascript" defer="defer" src="$(NAME).js"></script>
+  <title>Aš valdau HTML!</title>
+</head>
+<body>
+  <h1>Karolis - HTML asas!</h1>
+  <p>Moku kurti statinį turinį</p>
+
+</body>
+</html>
