1897732c00fc3b008cbe610192bd4393ea24ffa2
Naktinis-hash <57089674+Naktinis-hash@users.noreply.github.com>
2019-10-29 11:02:01 +0000

    php ir html

--- index.php
+++ index.php
@@ -1,19 +1,12 @@
-<!DOCTYPE html>
-<!--
-$(SRC_HEADER,indent("   "))
-  -->
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" media="screen, print"
-        href="$(NAME).css" />
-  <script type="text/javascript" defer="defer" src="$(NAME).js"></script>
-  <title>Aš valdau HTML!</title>
-</head>
-<body>
-  <h1>Karolis - HTML asas!</h1>
-  <p>Moku kurti statinį turinį</p>
 
-</body>
+
+<html>
+    <head>
+        <title>Aš valdau HTML <?php print ' ir PHP!'?></title>
+    </head>
+    <body>
+        <h1>Karolis - HTML<?php print ' ir PHP'?>asas!</h1>
+        <p>Moku kurti dinaminį turinį
+           Atidaryta: <?php print date('Y-m-d H-i-s')?></p>
+    </body>
 </html>
