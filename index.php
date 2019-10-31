5ea52085e257e06854974927e560a917df487134
Naktinis-hash <57089674+Naktinis-hash@users.noreply.github.com>
2019-10-31 11:28:18 +0000

    kabantys

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <?php
-$img_src = 'https://www.probytes.net/wp-content/uploads/2018/01/5-1.png'
+$sudas_1 = rand(1, 100);
+$sudas_2 = rand(101, 200);
+$sudas_3 = rand(201, 300);
+$sudas_4 = rand(301, 400);
+$h1 = 'Skolos skaiciuokle';
+$h3_1 = "Jei paemei:$sudas_1";
+$h3_2 = "Su dviem kabanciais grazinsi:$sudas_3";
+$h3_3 = "Su vienu kabanciu grazinsi:$sudas_4";
 ?>
 <html>
     <head>
-        <title>Taip</title>
-        <style>
-            .img{
-                width: 200px;
-                height: 200px;
-            }
-        </style>
+        <title>Kabantys</title>
     </head>
     <body>
-        <img src="<?php print $img_src; ?>">
-        <img src="<?php print $img_src; ?>">
-        <img src="<?php print $img_src; ?>">
-        <img src="<?php print $img_src; ?>">
+        <div>
+            <h1><?php print $h1; ?></h1>
+            <h3><?php print $h3_1; ?></h3>
+            <h3><?php print $h3_2; ?></h3>
+            <h3><?php print $h3_3; ?></h3>
+        </div>
     </body>
