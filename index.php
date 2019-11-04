41e454d49b93e507d63b79e892a8896c63222cd6
Naktinis-hash <57089674+Naktinis-hash@users.noreply.github.com>
2019-11-04 15:45:08 +0000

    bobelka

--- index.php
+++ index.php
@@ -1,24 +1,29 @@
 <?php
 $grizai_velai = rand(0, 1);
 $grizai_isgeres = rand(0, 1);
+$kur_miegosi = 'Nemiegosi';
 if ($grizai_velai && !$grizai_isgeres) {
-    $h2_eilute = 'grizai velai' ;
-    }  else if ($grizai_velai && $grizai_isgeres){
-        $h2_eilute = 'grizai isgeres ir velai';
-    } elseif (!$grizai_velai && $grizai_isgeres) {
-        $h2_eilute = 'Grizai isgeres';
-    } elseif (!$grizai_velai && !$grizai_isgeres) {
-        $h2_eilute = 'Nieko nepadariau, nemuskit';
-    }
-    $h1 = 'Buitine skaiciuokle';
-    $h2 = "Situacija: $h2_eilute";
-    ?>
-    <html>
-        <head>
-            <title>Pripiske</title>
-        </head>
-        <body>
-            <h1><?php print $h1; ?></h1>
-            <h2> <?php print $h2; ?></h2>
+    $h2_eilute = 'grizai velai';
+} elseif ($grizai_velai && $grizai_isgeres) {
+    $h2_eilute = 'grizai isgeres ir velai';
+    $kur_miegosi = 'Miegosi';
+} elseif (!$grizai_velai && $grizai_isgeres) {
+    $h2_eilute = 'Grizai isgeres';
+} elseif (!$grizai_velai && !$grizai_isgeres) {
+    $h2_eilute = 'Nieko nepadariau, nemuskit';
+}
+$h1 = 'Buitine skaiciuokle';
+$h2 = "Situacija: $h2_eilute";
+$h3 = "Isvada : $kur_miegosi ant sofos";
+?>
+<html>
+    <head>
+        <title>Pripiske</title>
+    </head>
+    <body>
+        <h1><?php print $h1; ?></h1>
+        <h2> <?php print $h2; ?></h2>
+        <h3><?php print $h3; ?></h3>
+        <img src="https://i.ytimg.com/vi/chFTjHpHAEM/mqdefault.jpg">
     </body>
 </html>
