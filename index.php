35ae7cc9cb12987967caa1bd55815059e637106e
Naktinis-hash <57089674+Naktinis-hash@users.noreply.github.com>
2019-11-04 15:10:23 +0000

    girtas

--- index.php
+++ index.php
@@ -1,36 +1,24 @@
 <?php
-$distance = rand(500, 1000);
-$consumption = 7.5;
-$price_1 = 1.3;
-$fuel_total = round($distance * ($consumption / 100), 2);
-$price_trip = round($fuel_total * $price_1, 2);
-$my_money = 100;
-$h1 = 'Keliones skaiciuokle';
-$li_1 = "Nuvaziuota distancija $distance";
-$li_2 = "Sunaudota $fuel_total l kuro";
-$li_3 = "Kaina $price_trip pinigu";
-$li_4 = "Turimi pinigai $my_money";
-if ($price_trip <= "100") {
-    $kaina = 'Iperkama';
-} elseif ($price_trip > "100") {
-    $kaina = 'Neiperkama';
-}
-$p = "Isvada: kelione $kaina";
-
-?>
-<html>
-    <head>
-        <title>Kuras</title>
-    </head>
-    <body>
-        <h1><?php print $h1; ?></h1>
-        <ul>
-            <li><?php print $li_1; ?></li>  
-            <li><?php print $li_2; ?></li>  
-            <li><?php print $li_3; ?></li> 
-            <li><?php print $li_4; ?></li>
-        </ul>
-        <hr>
-        <p><?php print $p; ?></p>
+$grizai_velai = rand(0, 1);
+$grizai_isgeres = rand(0, 1);
+if ($grizai_velai && !$grizai_isgeres) {
+    $h2_eilute = 'grizai velai' ;
+    }  else if ($grizai_velai && $grizai_isgeres){
+        $h2_eilute = 'grizai isgeres ir velai';
+    } elseif (!$grizai_velai && $grizai_isgeres) {
+        $h2_eilute = 'Grizai isgeres';
+    } elseif (!$grizai_velai && !$grizai_isgeres) {
+        $h2_eilute = 'Nieko nepadariau, nemuskit';
+    }
+    $h1 = 'Buitine skaiciuokle';
+    $h2 = "Situacija: $h2_eilute";
+    ?>
+    <html>
+        <head>
+            <title>Pripiske</title>
+        </head>
+        <body>
+            <h1><?php print $h1; ?></h1>
+            <h2> <?php print $h2; ?></h2>
     </body>
 </html>
