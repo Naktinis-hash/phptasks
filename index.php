38897a3b6b5a9ce673c333d6504c5696f0fe2297
Naktinis-hash <57089674+Naktinis-hash@users.noreply.github.com>
2019-11-05 15:00:14 +0000

    orgija

--- index.php
+++ index.php
@@ -1,14 +1,29 @@
 <?php
-$x = rand(0, 10);
-for ($y = 0; $y < $x; $y++) {
-    var_dump("Tai yra $y - tasis ciklas");
+$kates = rand(1, 3);
+$sunys = rand(1, 3);
+$pavyko = rand(0, 1);
+$katasuniai = 0;
+
+for ($c = 1; $c <= $kates; $c++) {
+    for ($d = 1; $d <= $sunys; $d++){
+        $pavyko = rand(0, 1);
+        if ($pavyko);
+        $katasuniai++;
+        break;
+    }
 }
-$title = 'Ciklai'
+$title = 'Orgija';
+$h1 = 'Katasuniu iseiga';
+$h2 = "Dalyvavo $kates kates ir $sunys sunys";
+$h3 = "Katasuniu iseiga: $katasuniai";
 ?>
 <html>
     <head>
         <title><?php print $title; ?></title>
     </head>
     <body>
+        <h1><?php print $h1; ?></h1>
+        <h2><?php print $h2; ?></h2>
+        <h3><?php print $h3; ?></h3>
     </body>
 </html>
