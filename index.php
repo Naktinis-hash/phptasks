2f06531e3917d0b38ca4cdde37836661c81aa2fb
Naktinis-hash <57089674+Naktinis-hash@users.noreply.github.com>
2019-11-05 15:49:08 +0000

    islaidos

--- index.php
+++ index.php
@@ -1,21 +1,15 @@
 <?php
-$kates = rand(1, 3);
-$sunys = rand(1, 3);
-$pavyko = rand(0, 1);
-$katasuniai = 0;
-
-for ($c = 1; $c <= $kates; $c++) {
-    for ($d = 1; $d <= $sunys; $d++){
-        $pavyko = rand(0, 1);
-        if ($pavyko);
-        $katasuniai++;
-        break;
-    }
+$months = 12;
+$wallet = 1000;
+$month_income = 700;
+$month_expenses = rand(1, 1000);
+for ($i = 1; $i <= $months; $i++) {
+    $months_expenses = rand(1, 1000);
+    $wallet += $month_income - $month_expenses;
 }
-$title = 'Orgija';
-$h1 = 'Katasuniu iseiga';
-$h2 = "Dalyvavo $kates kates ir $sunys sunys";
-$h3 = "Katasuniu iseiga: $katasuniai";
+$title = 'Pinigine';
+$h1 = 'Wallet forecast';
+$h2 = "Po $months m., prognozuojamas likutis: $wallet";
 ?>
 <html>
     <head>
@@ -24,6 +18,5 @@ $h3 = "Katasuniu iseiga: $katasuniai";
     <body>
         <h1><?php print $h1; ?></h1>
         <h2><?php print $h2; ?></h2>
-        <h3><?php print $h3; ?></h3>
     </body>
 </html>
