35416f8fe0081d2beed6e1077329b4776362a94f
Naktinis-hash <57089674+Naktinis-hash@users.noreply.github.com>
2019-11-05 14:38:47 +0000

    ciklai

--- index.php
+++ index.php
@@ -1,39 +1,14 @@
 <?php
-$sunny = rand(0, 1);
-if ($sunny) {
-    $h1 = 'Sauleta';
-    $class = 'bg-sunny';
-} else {
-    $h1 = 'Debesuota su lietum';
-    $class = 'bg-rainy';
+$x = rand(0, 10);
+for ($y = 0; $y < $x; $y++) {
+    var_dump("Tai yra $y - tasis ciklas");
 }
+$title = 'Ciklai'
 ?>
 <html>
     <head>
         <title><?php print $title; ?></title>
     </head>
-    <style>
-        .content {
-            display: flex;
-            flex-direction: row;
-            align-items: center;
-        }
-        .bg-img {
-            width: 150px;
-            height: 150px;
-            background-size: cover;
-        }
-        .bg-sunny {
-            background-image: url("https://images.vexels.com/media/users/3/145134/isolated/preview/46b65a02ff99e7bb4e84d4d3d627a729-sun-sharp-beams-icon-by-vexels.png");
-        }
-        .bg-rainy {
-            background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGJheJPOzKdEI92PRAjtuz8S7a9YU-UZl1awnwyyEFdWe8V_Ju&s");
-        }
-    </style>
     <body>
-        <div class="content">
-            <div class="bg-img <?php print $class; ?>"></div>
-            <h1><?php print $h1; ?></h1>
-        </div>
     </body>
 </html>
