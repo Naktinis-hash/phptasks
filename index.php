51e4abf9f4764579888fdfd8ca0104c67c260bfc
Naktinis-hash <57089674+Naktinis-hash@users.noreply.github.com>
2019-11-07 16:35:49 +0000

    baulingas

--- index.php
+++ index.php
@@ -5,28 +5,49 @@ $array = [
     //Neprivemtas 
     [
         [
+            'busenas' => 'Neprivemtas',
+        ],
+        '1 eile' => [
             'skyle1' => true,
             'skyle2' => false,
             'skyle3' => false,
             'skyle4' => true,
         ],
-        [
+        '2 eile' =>[
             'skyle1' => false,
             'skyle2' => false,
             'skyle3' => true,
         ],
-        [
+        '3 eile' => [
             'skyle1' => false,
             'skyle2' => true,
         ],
-        [
+        '4 eile' => [
             'skyle1' => true,
         ],
     ],
     //Privemtas 
     [
         [
-            'skyles' => 'Nenaudojamos del techniniu problemu'
+            'busena' => 'Privemtas',
+        ],
+                [
+            'skyle1' => false,
+            'skyle2' => false,
+            'skyle3' => false,
+            'skyle4' => false,
+        ],
+        [
+            'skyle1' => false,
+            'skyle2' => false,
+            'skyle3' => false,
+        ],
+        [
+            'skyle1' => false,
+            'skyle2' => false,
+        ],
+        [
+            'skyle1' => false,
         ],
     ],
 ];
