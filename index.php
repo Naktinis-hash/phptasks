91c80362401e463ec9ae5ae674111333af3631ea
Naktinis-hash <57089674+Naktinis-hash@users.noreply.github.com>
2019-11-07 15:59:28 +0000

    array

--- index.php
+++ index.php
@@ -1,32 +1,46 @@
 <?php
-$pjuklas = [
-    ['individas' => [
-        'vardas' => 'Petras',
-        'pavarde' => 'Pizdys'
+ini_set('xdebug.var_display_max_depth', '10');
+ini_set('xdebug.var_display_max_children', '256');
+ini_set('xdebug.var_display_max_data', '1024');
+
+$array = [
+    [
+        [
+            'produktas' => 'kiausiniai',
+            'kiekis' => 3,
+        ],
+        [
+            'produktas' => 'grietine',
+            'kiekis' => 1,
+        ],
+        [
+            'produktas' => 'jogurtas',
+            'kiekis' => 2,
+        ],
+        [
+             'produktas' => 'zuvis',
+             'kiekis' => 0,
+        ],
     ],
-    'zudymo irankis' => [
-        'koks' => 'Prapistas',
-        'daiktas' => 'telefonas'
+    [
+        [
+            'produktas' => 'pienas',
+            'kiekis' => 1,
+        ],
+        [
+            'produktas' => 'kefyras',
+            'kiekis' => 0,
+        ],
+        [
+            'produktas' => 'degtine',
+            'kiekis' => 2,
+        ],
+        [
+            'produktas' => 'kecupas',
+            'kiekis' => 2,
+        ],
     ],
-],       
-    ['individas' => [
-        'vardas' => 'Tomas',
-        'pavarde' => 'Ablomas'
-    ],
-    'zudymo irankis' => [
-        'koks' => 'naudojamas',
-        'daiktas' => 'buljonas'
-    ],
-],        
-    ['individas' => [
-        'vardas' => 'Ana',
-        'pavarde' => 'Shitkova'
-    ],
-    'zudymo irankis' => [
-        'koks' => 'pasibaiges',
-        'daiktas' => 'sikpopierius'
-    ]
-],        
+     
 ];
-var_dump($pjuklas);
- 
+var_dump($array);
+
