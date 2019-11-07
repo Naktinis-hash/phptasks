8e10b47fec567da2582ec45a50bff15871fc2b05
Naktinis-hash <57089674+Naktinis-hash@users.noreply.github.com>
2019-11-07 16:43:24 +0000

    bowling

--- index.php
+++ index.php
@@ -2,54 +2,54 @@
 
 //Skaiciuojame nuo 4 kegliu eiles, tada 3kegliu ir t.t
 $array = [
-    //Neprivemtas 
-    [
-        [
-            'busenas' => 'Neprivemtas',
-        ],
-        '1 eile' => [
-            1 => true,
-            2 => false,
-            3 => false,
-            4 => true,
-        ],
-        '2 eile' => [
-            1 => false,
-            2 => false,
-            3 => true,
-        ],
-        '3 eile' => [
-            1 => false,
-            2 => true,
-        ],
-        '4 eile' => [
-            1 => true,
-        ],
-    ],
-    //Privemtas 
-    [
-        [
-            'busena' => 'Privemtas',
-        ],
-        [
-            1 => false,
-            2 => false,
-            3 => false,
-            4 => false,
-        ],
-        [
-            1 => false,
-            2 => false,
-            3 => false,
-        ],
-        [
-            1 => false,
-            2 => false,
-        ],
-        [
-            1 => false,
-        ],
-    ],
+//Neprivemtas 
+[
+'Neprivemtas' => [
+'busena' => 'Kegliai',
+],
+ 1 => [
+1 => true,
+ 2 => false,
+ 3 => false,
+ 4 => true,
+],
+ 2 => [
+1 => false,
+ 2 => false,
+ 3 => true,
+],
+ 3 => [
+1 => false,
+ 2 => true,
+],
+ 4 => [
+1 => true,
+],
+],
+ //Privemtas 
+[
+'Privemtas' => [
+'busena' => 'Kegliai',
+],
+ 1[
+1 => false,
+ 2 => false,
+ 3 => false,
+ 4 => false,
+],
+ 2[
+1 => false,
+ 2 => false,
+ 3 => false,
+],
+ 3[
+1 => false,
+ 2 => false,
+],
+ 4[
+1 => false,
+],
+],
 ];
 var_dump($array);
 
