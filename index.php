5d9bf65001d021ab95e36d33d9d0d25e8d57236b
Naktinis-hash <57089674+Naktinis-hash@users.noreply.github.com>
2020-02-24 12:17:15 +0000

    sugrizimas

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
-
 $date = date('Y-m-d H:i:s');
 
-$x = '$date : Karolis grizo i phpfightcluba!';
+$p = "$date : Karolis grizo i phpfightcluba!";
 ?>
 
 <html>
     <body>
-        <p> <?php print $date ?> : Karolis grizo i PHPFIGHTCLUB'a!</p>
+        <p> <?php print $p ?></p>
     </body>
 </html>
